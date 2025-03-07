b6641429ce5649d65a433968b189003d36343cbf
piyush goyal <piygoyal30@gmail.com>
2025-03-07 15:53:47 +0530

    [Piyush Goyal] solved uc_pin

---
+++ UC3ValidatePinCode.js
@@ -0,0 +1,7 @@
+const generalPinRegex = /^[1-9][0-9]{5}$/;
+
+console.log(generalPinRegex.test("400088")); 
+console.log(generalPinRegex.test("000123")); 
+console.log(generalPinRegex.test("98765")); 
+console.log(generalPinRegex.test("1234567"));
+console.log(generalPinRegex.test("400088B")); 
